bd5f07ab049e1d0594662d0e731b05fd402f5253
Vassiliou Petros <vassiliou.g.petros@gmail.com>
2013-11-20 22:51:49 +0200

    Add Draw functions for FxMatrixF. Fix FxContour algorithm.

--- FxMaths/Matrix/FxContour.cs
+++ FxMaths/Matrix/FxContour.cs
@@ -49,20 +49,20 @@ namespace FxMaths.Matrix
             ChainList = new List<FxContourChain>();
 
             int labelCount = 1;
-            for (int i = 1; i < maskSize -1; i++)
+            var maskG = mask.ToFxMatrixF().Gradient(FxMatrixF.GradientMethod.Roberts) > 0;
+            for (int i = 1; i < maskSize - 1; i++)
             {
                 /* check if we have edge */
-                if((mask[i] ^ mask[i+1]) && (labelMap[i] + labelMap[i+1] == 0))
+                if (maskG[i])
                 {
                     int x;
                     int y = Math.DivRem(i, mask.Width, out x);
                     labelMap[x, y] = labelCount;
-                    remainMask[x, y] = false;
+                    maskG[x, y] = false;
 
                     // create a new chain 
                     FxContourChain newChain = new FxContourChain(x, y);
 
-#if true
                     /* propacate the search in sub pixels */
                     Labeling_addStack(stack, x, y);
                     while (stack.Count > 0)
@@ -74,124 +74,37 @@ namespace FxMaths.Matrix
                         if (x < 1 || (x >= mask.Width - 1) || y < 1 || (y >= mask.Height - 1))
                             continue;
 
-                       // if (newChain.StartPoint.x == x && newChain.StartPoint.y == y)
-                        //    break;
-
-                        if (remainMask[x, y] && check(mask, x, y))
+                        if (maskG[x, y] && check(maskG, x, y))
                         {
                             // add the labeling
                             labelMap[x, y] = labelCount;
-                            remainMask[x, y] = false;
-                            
+                            maskG[x, y] = false;
+
                             // add the new point to the chain
                             newChain.PushPoint(x, y);
 
                             Labeling_addStack(stack, x, y);
                         }
                     }
-#else
-                    // the first point that we are finding is having only from x + 1, y and clockwise 
-
-                    int oldX = x, oldY = y;
-                    int oldPath = 0; // we have 8 paths
-                    bool cw = false;
-
-                    if (mask[x + 1, y] == true) { x++; oldPath = 8; }
-                    else if (mask[x + 1, y + 1] == true) { x++; y++; oldPath = 1; }
-                    else if (mask[x, y + 1] == true) { y++; oldPath = 2; }
-                    else if (mask[x - 1, y + 1] == true) { x--; y++; oldPath = 3; }
-                    else throw new Exception();
-                    
-                    while (true)
-                    {
-                        if (x < 1 || (x >= mask.Width - 1) || y < 1 || (y >= mask.Height - 1))
-                            continue;
-
-                        if (newChain.StartPoint.x == x && newChain.StartPoint.y == y)
-                            break;
-
-                        // add the labeling
-                        labelMap[x, y] = labelCount;
-                        remainMask[x, y] = false;
-
-                        // add the new point to the chain
-                        newChain.PushPoint(x, y);
-
-                        // calc the new point
-
-                        // 8 cases
-                        switch(oldPath)
-                        {
-                            case 1:
-                                // select the if we move clockwise
-                                if (mask[oldX + 1, oldY])
-                                    cw = true;
-                                else if (mask[oldX, oldY + 1])
-                                    cw = false;
-                                else
-                                    throw new Exception();
-
-                                if(cw)
-                                {
-                                    if (mask[x - 1, y - 1] == true) { x++; oldPath = 8; }
-                                    else if (mask[x + 1, y + 1] == true) { x++; y++; oldPath = 1; }
-                                    else if (mask[x, y + 1] == true) { y++; oldPath = 2; }
-                                    else if (mask[x - 1, y + 1] == true) { x--; y++; oldPath = 3; }
-                                    else throw new Exception();
-                                }
-                                else
-                                {
-
-                                }
-
-
-
-                                break;
-                            case 2:
-
-                                break;
-                            case 3:
-
-                                break;
-                            case 4:
-
-                                break;
-                            case 5:
-
-                                break;
-                            case 6:
-
-                                break;
-                            case 7:
-
-                                break;
-                            case 8:
-
-                                break;
-                        }
-
-
-                        // save old points
-                        oldX = x; oldY = y;
-                    }
-#endif
                     labelCount++;
+
+                    // calc boundary rect
 
                     // add the new chain to the list 
                     ChainList.Add(newChain);
                 }
             }
-
         }
 
         public FxMatrixF ToFxMatrixF(int Width, int Height)
         {
             FxMatrixF result = new FxMatrixF(Width, Height);
-
-            for (int i = 0; i < NumChains; i++)
+            int i = 0;
+            foreach(FxContourChain chain in ChainList)
             {
-                FxVector2f prevPoint = ChainList[i].StartPoint;
-                foreach (FxComplexF c in ChainList[i].ListComplex)
+                i++;
+                FxVector2f prevPoint = chain.StartPoint;
+                foreach (FxComplexF c in chain.ListComplex)
                 {
                     result[(int)prevPoint.x, (int)prevPoint.y] = i;
 
@@ -199,6 +112,9 @@ namespace FxMaths.Matrix
                     prevPoint.x += c.r;
                     prevPoint.y += c.i;
                 }
+
+                // Draw boundrary rect
+                result.DrawRect(chain.RectStart, chain.RectSize, i);
             }
 
             result.Divide(result.Max());
@@ -224,9 +140,21 @@ namespace FxMaths.Matrix
 
 
         /// <summary>
+        /// The start of the rect boundary.
+        /// </summary>
+        public FxVector2f RectStart;
+
+        /// <summary>
+        /// The size of the rect.
+        /// </summary>
+        public FxVector2f RectSize;
+
+        /// <summary>
         /// The previous added point
         /// </summary>
         private FxVector2f prevPoint;
+
+        
 
         public FxContourChain(float x, float y)
         {
@@ -236,6 +164,9 @@ namespace FxMaths.Matrix
 
             // create the complex list 
             ListComplex = new List<FxComplexF>();
+
+            RectStart = new FxVector2f(x, y);
+            RectSize = new FxVector2f(0, 0);
         }
 
         public void PushPoint(float x, float y)
@@ -246,6 +177,28 @@ namespace FxMaths.Matrix
             }
             prevPoint.x = x;
             prevPoint.y = y;
+
+
+            // Renew rect boundary
+            if (RectStart.x > x)
+            {
+                RectSize.x += RectStart.x - x;
+                RectStart.x = x;
+            }
+            else if (RectStart.x + RectSize.x < x)
+            {
+                RectSize.x = x - RectStart.x;
+            }
+
+            if (RectStart.y > y)
+            {
+                RectSize.y += RectStart.y - y;
+                RectStart.y = y;
+            }
+            else if (RectStart.y + RectSize.y < y)
+            {
+                RectSize.y = y - RectStart.y;
+            }
         }
 
         public FxVector2f PopPoint()
---
+++ FxMaths/Matrix/FxMatrixF.Draw.cs
@@ -0,0 +1,66 @@
+﻿using FxMaths.Vector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FxMaths.Matrix
+{
+    public partial class FxMatrixF
+    {
+        /// <summary>
+        /// Draw a line with specific value
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        public void DrawLine(FxVector2f start, FxVector2f end, float value)
+        {
+            var dx = Math.Abs(end.x - start.x);
+            var dy = Math.Abs(end.y - start.y);
+            var sx = (start.x < end.x) ? 1 : -1;
+            var sy = (start.y < end.y) ? 1 : -1;
+            var err = dx - dy;
+            int x0 = (int)start.x;
+            int y0 = (int)start.y;
+            int x1 = (int)end.x;
+            int y1 = (int)end.y;
+            while(!((x0 == x1) && (y0 == y1)))
+            {
+                this[x0, y0] = value;
+                var e2 = 2 * err;
+                if (e2 > -dy)
+                {
+                    err -= dy;
+                    x0 += sx;
+                }
+
+                if ((x0 == x1) && (y0 == y1))
+                {
+                    this[x0, y0] = value;
+                    break;
+                }
+
+                if (e2 < -dx)
+                {
+                    err -= dx;
+                    y0 += sy;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Draw Rectangle.
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="size"></param>
+        public void DrawRect(FxVector2f start, FxVector2f size, float value)
+        {
+            DrawLine(start, new FxVector2f(start.x + size.x, start.y), value);
+            DrawLine(new FxVector2f(start.x + size.x, start.y), new FxVector2f(start.x + size.x, start.y + size.y), value);
+            DrawLine(new FxVector2f(start.x + size.x, start.y + size.y), new FxVector2f(start.x, start.y + size.y), value);
+            DrawLine(new FxVector2f(start.x, start.y + size.y), start, value);
+        }
+
+    }
+}
